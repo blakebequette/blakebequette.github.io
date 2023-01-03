1a0d2bc7a52f7544a15eb136ce65138a35d116e4
beq <blakebequette@gmail.com>
2023-01-02 23:46:56 -0600

    add campus manager, new photo url

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const projectList = [
     {
         projectName: "Coffee Clicker",
         description: "Vanilla DOM brute force clicking game",
-        thumbnail: "https://i.ibb.co/4WfwMj6/coffee-Clicker.png",
+        thumbnail: "https://i.postimg.cc/dVWNYrCv/coffee-Clicker.png",
         entry: "/projects/coffeeClicker/index.html"
     },
     {
         projectName: "Whack-A-Mole",
         description: "Vanilla DOM whack-a-mole game with some event listener improvisation",
-        thumbnail: "https://i.ibb.co/k8RVDsR/whack-AMole.png",
+        thumbnail: "https://i.postimg.cc/t4gD2JMc/whack-AMole.png",
         entry: "/projects/whackAMole/index.html"
     },
     {
@@ -16,6 +16,12 @@ const projectList = [
         description: "Mapping the first circumnavigation via plane, using React and Leaflet",
         thumbnail: "https://i.ibb.co/Pj0MgF6/leaflet-pic.png",
         entry: "projects/mapfun/index.html"
+    },
+    {
+        projectName: "Campus Manager - CRUD App",
+        description: "Campus manager application. Front end: React & Redux, Back end: Express + Postgres",
+        thumbnail: "https://i.postimg.cc/MTJ72p9q/campus-Manager.png",
+        entry: "https://youtu.be/csAtX-6leZw"
     }
 ]
 // <a href="https://ibb.co/LSPD6CL"><img src="https://i.ibb.co/4WfwMj6/coffee-Clicker.png" alt="coffee-Clicker" border="0" /></a>
@@ -30,8 +36,11 @@ function project2TileNode(project){
         <h4 class="project-title">${project.projectName}</h4>
         <p class="project-description">${project.description}</p>`
     newTile.addEventListener("click", ()=> {
-        console.log(window.location)
-        window.location = window.location + `${project.entry}`
+        if (project.projectName === "Campus Manager - CRUD App") {
+            window.location = project.entry
+        } else {
+            window.location = window.location + `${project.entry}`
+        }
     })
     return newTile
 }
